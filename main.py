74c970717d07627da58ec8a9d2dba29282c57fbc
Mark Muchane <mark@pztron.io>
2020-12-02 16:01:51 -0500

    save

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as pyplot
 import numpy as np
-import pundas as pd
+import pandas as pd
 
 data = pd.read_csv("data.csv")
-
+patient_id = data["Patient"]
+consent = data[""]
